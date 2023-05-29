70da63245b5b5011a49d90dc41ad2f0aaf4d7060
HK9901 <akadora234w@gmail.com>
2023-05-29 10:41:07 +0900

    作業中断

---
+++ web/react-app/src/components/App.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+// import { BrowserRouter as Router, Route, useNavigate } from './react-router-dom';
+import './use_terms.css';
+
+
+const Account: React.FC = () => {
+  return (
+    <>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>アカウント登録</title>
+      </head>
+      <header>モバリス加入店規約</header>
+      <body>
+        <label className="">利用規約</label>
+       </body>
+    </>
+  );
+};
+
+export default Account;
+
---
+++ web/react-app/src/components/account.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Account: React.FC= () => {
+  return (
+    <div>
+      <h1>Account Page</h1>
+      {/* アカウントページのコンテンツをここに追加 */}
+    </div>
+  );
+}
+
+export default Account;
