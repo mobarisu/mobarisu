981651161192be37c492ec90ec6c5e7d606e1ae1
HK9901 <akadora234w@gmail.com>
2023-05-30 14:10:38 +0900

    account完成

---
+++ web/react-app/src/components/confirmation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Confirmation: React.FC = () => {
+  // コンポーネントの実装を記述します
+
+  return (
+    <div>
+     <h1>メールアドレス確認画面</h1>
+    </div>
+  );
+};
+
+export default Confirmation;
