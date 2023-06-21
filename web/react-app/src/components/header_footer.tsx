bc8da507cd8cdcd839f8bccce3a517ab59c38b79
nakaneakira <baka101566@gmail.com>
2023-06-21 11:09:55 +0900

    客側header作成　カートの方はカウントが必要なためエラーを吐く

--- web/react-app/src/components/header_footer.tsx
+++ web/react-app/src/components/header_footer.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import logo from './image/logo.png';
-// import cart from "./image/cart.jpeg";
-// import food from "./image/food.jpg";
+import cart from "./image/cart.jpeg";
 
 
 export const StoreHeader: React.FC = () =>{
+  const navigate = useNavigate();
     return (
       // 店側header
         <header className='header_ac_on'>
@@ -16,17 +17,22 @@ export const StoreHeader: React.FC = () =>{
 };
 
 export const FoodHeader: React.FC = () =>{
+  const navigate = useNavigate();
     return (
       //客側header
-        <header className='header_ac_on'>
-        <div className="header-logo">
-          <a href="#"><img src={logo} className="logo" alt=''/></a>
-        </div>
-      </header>
+      <header className='header_ac_on'>
+      <div className="header-logo">
+          <a onClick={() => navigate("/food_list")}><img src={logo} className="logo" alt=''/></a>
+      </div>
+      <div className="header-cart">
+          <a onClick={() => navigate("/food_cart")}><img src={cart} className="cart" alt='カート'/></a>
+      </div>
+  </header>
     );
 };
 
 export const StoreFooter: React.FC = () =>{
+  const navigate = useNavigate();
   return (
     //店側footer
       <footer>
@@ -36,6 +42,7 @@ export const StoreFooter: React.FC = () =>{
 };
 
 export const FoodFooter: React.FC = () =>{
+  const navigate = useNavigate();
   return (
     //客側footer
     <footer>
