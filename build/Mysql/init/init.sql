21663d6ab3f503010fba653284b63503554145b9
mokotya <mokotya0108@outlook.jp>
2023-06-22 14:07:11 +0900

    挙動確認完了

--- build/Mysql/init/init.sql
+++ build/Mysql/init/init.sql
@@ -12,7 +12,7 @@ create table stores(
     mail_address varchar(255) NOT NULL,
     phone_number long NOT NULL,
     representative_name varchar(128) NOT NULL,
-    path varchar(255) NOT NULL,
+    store_path varchar(255) NOT NULL,
     password varchar(255) NOT NULL
 );
 
@@ -35,7 +35,7 @@ create table products(
     product_name varchar(255) NOT NULL,
     product_introduction varchar(255),
     product_price int NOT NULL,
-    path varchar(255) NOT NULL,
+    product_path varchar(255) NOT NULL,
     on_sale boolean NOT NULL,
     FOREIGN KEY (store_id) REFERENCES stores(store_id),
     FOREIGN KEY (category_id) REFERENCES products_categories(category_id)
@@ -52,7 +52,7 @@ create table products_options(
 create table allergies(
     allergy_id int PRIMARY KEY AUTO_INCREMENT,
     ingredient varchar(64) NOT NULL,
-    path varchar(255) NOT NULL
+    allergy_path varchar(255) NOT NULL
 );
 
 create table products_allergies(
@@ -92,7 +92,7 @@ values
 ('ドリンク');
 
 insert into allergies
-(ingredient,path)
+(ingredient,allergy_path)
 values
 ('卵','/allergie/egg.svg'),
 ('乳','/allergie/milk.svg'),
@@ -129,27 +129,27 @@ values
 ('サイズなし',0);
 
 insert into stores
-(store_name,store_introduction,mail_address,phone_number,representative_name,password)
+(store_name,store_introduction,mail_address,phone_number,representative_name,store_path,password)
 values ('マクドナルド','世界的なファストフードチェーンで、美味しいハンバーガーやポテトなど幅広いメニューを提供。快適な空間で楽しい食事体験をお届けします。',
 'mac@gmail.com',00012340123,'ドナルドマクドナルド','/store/1.jpg','Pass_1234_qwaszx');
 
 insert into stores
-(store_name,store_introduction,mail_address,phone_number,representative_name,password)
+(store_name,store_introduction,mail_address,phone_number,representative_name,store_path,password)
 values ('モスバーガー','新鮮な素材で作る美味しいバーガーやサイドメニューを提供。心地よい空間でお楽しみいただけます。ご家族や友人との食事に最適です。',
 'mos@gmail.com',02323450123,'モス','/store/2.jpg','Qwerty_1234_pass');
 
 insert into stores
-(store_name,store_introduction,mail_address,phone_number,representative_name,password)
+(store_name,store_introduction,mail_address,phone_number,representative_name,store_path,password)
 values ('すき家','新鮮な食材を使った美味しいすき焼きや丼物をリーズナブルに提供。心地よい雰囲気でおくつろぎください。ご家族や友人との食事に最適です。',
 'sukiya@gmail.com',02342345123,'スキヤ','/store/3.jpg','Qwaszx_1234_pass');
 
 insert into stores
-(store_name,store_introduction,mail_address,phone_number,representative_name,password)
+(store_name,store_introduction,mail_address,phone_number,representative_name,store_path,password)
 values ('松屋','美味しい牛丼や定食をリーズナブルに提供。忙しい日常に手軽でヘルシーな食事をお届けします。心地よい雰囲気でおくつろぎください。ご来店をお待ちしています！',
 'matuya@gmail.com',09871234098,'マツヤ','/store/4.jpg','QWerasd_10293_pass');
 
 insert into products
-(store_id,category_id,product_name,product_introduction,product_price,on_sale)
+(store_id,category_id,product_name,product_introduction,product_price,product_path,on_sale)
 values (1,1,'炙り醤油風ダブル肉厚ビーフ','2枚重ねた厚みのある100%ビーフのおいしさを、香ばしい炙り醤油風のソースが引き立てる、思わずやみつきになる一品です。',
 550,'/product/1.jpg',true);
 
@@ -171,7 +171,7 @@ values
 (1,1);
 
 insert into products
-(store_id,category_id,product_name,product_introduction,product_price,on_sale)
+(store_id,category_id,product_name,product_introduction,product_price,product_path,on_sale)
 values (1,1,'炙り醤油風ベーコントマト肉厚ビーフ','厚みのある100%ビーフとふんだんな野菜に、香ばしい炙り醤油風のソースが決め手の、肉と野菜のバランスが絶妙な一品です。',
 540,'/product/2.jpg',true);
 
@@ -193,7 +193,7 @@ values
 (2,1);
 
 insert into products
-(store_id,category_id,product_name,product_introduction,product_price,on_sale)
+(store_id,category_id,product_name,product_introduction,product_price,product_path,on_sale)
 values (1,1,'チキンフィレオ','ボリュームたっぷりのムネ肉を使ったチキンパティで、食べ応えもばっちり。オーロラソースとの相性も抜群のサンドイッチです。',
 380,'/product/3.jpg',true);
 
@@ -215,7 +215,7 @@ values
 (3,1);
 
 insert into products
-(store_id,category_id,product_name,product_introduction,product_price,on_sale)
+(store_id,category_id,product_name,product_introduction,product_price,product_path,on_sale)
 values (1,1,'ゆず香るおろしチキンタツタ','国産ゆずなどの柑橘でさっぱり仕上げた角切り大根入りのおろしと、たまり醤油マヨソースが相性抜群のチキンタツタが新登場！',
 460,'/product/4.jpg',false);
 
@@ -237,7 +237,7 @@ values
 (4,1);
 
 insert into products
-(store_id,category_id,product_name,product_introduction,product_price,on_sale)
+(store_id,category_id,product_name,product_introduction,product_price,product_path,on_sale)
 values (1,2,'マックフライポテト','外はカリッとゴールデンブラウン。中はホクホクとベイクドポテトのような食感。こだわりぬいた、マクドナルドのベストセラー。',
 330,'/product/5.jpg',true);
 
@@ -262,7 +262,7 @@ values
 (5,20);
 
 insert into products
-(store_id,category_id,product_name,product_introduction,product_price,on_sale)
+(store_id,category_id,product_name,product_introduction,product_price,product_path,on_sale)
 values (1,2,'サイドサラダ','レタス、紫キャベツ、赤と黄色のパプリカを組み合わせた彩り豊かなサラダです。ドレッシングはお好みに合わせてお選びください。',
 300,'/product/6.jpg',true);
 
@@ -272,7 +272,7 @@ values
 (6,1);
 
 insert into products
-(store_id,category_id,product_name,product_introduction,product_price,on_sale)
+(store_id,category_id,product_name,product_introduction,product_price,product_path,on_sale)
 values (1,2,'えだまめコーン','栄養バランスを考慮してスイートコーンにえだまめをプラスした、おいしく栄養を摂っていただけるメニューです。',
 250,'/product/7.jpg',true);
 
@@ -287,7 +287,7 @@ values
 (7,20);
 
 insert into products
-(store_id,category_id,product_name,product_introduction,product_price,on_sale)
+(store_id,category_id,product_name,product_introduction,product_price,product_path,on_sale)
 values (2,1,'ダブルとびきりチーズ ～北海道産ゴーダチーズ使用～','国産肉（牛・豚合挽き肉）を100％使用したハンバーグに、国産丸大豆醤油を使用したコクと深みのある“和風ソース”をかけました。',
 800,'/product/8.jpg',true);
 
@@ -306,7 +306,7 @@ values
 (8,27);
 
 insert into products
-(store_id,category_id,product_name,product_introduction,product_price,on_sale)
+(store_id,category_id,product_name,product_introduction,product_price,product_path,on_sale)
 values (2,1,'グリーンバーガー＜テリヤキ＞ GREEN BURGER TERIYAKI','やさしい甘みのベジタブルバンズと、大豆由来の原料を使用したパティを合わせたバーガーに、別添えのグリーンテリマヨソースをかけながらお召しあがりください。',
 590,'/product/9.jpg',true);
 
@@ -329,7 +329,7 @@ values
 (9,1);
 
 insert into products
-(store_id,category_id,product_name,product_introduction,product_price,on_sale)
+(store_id,category_id,product_name,product_introduction,product_price,product_path,on_sale)
 values (2,1,'とびきりトマト＆レタス','国産肉（牛・豚合挽き肉）を100％使用したハンバーグに、モスオリジナルの“和風ソース”をかけました。和風ソースが引き立てた、モスのこだわりがたっぷりつまった一品です。',
 550,'/product/10.jpg',true);
 
@@ -353,7 +353,7 @@ values
 (10,1);
 
 insert into products
-(store_id,category_id,product_name,product_introduction,product_price,on_sale)
+(store_id,category_id,product_name,product_introduction,product_price,product_path,on_sale)
 values (2,3,'アイスコーヒー','甘くなめらかな口当たりが特長で、まろやかな苦みとすっきりとした味わいのアイスコーヒーです。',
 320,'/product/12.jpg',true);
 
@@ -372,7 +372,7 @@ values
 (11,7);
 
 insert into products
-(store_id,category_id,product_name,product_introduction,product_price,on_sale)
+(store_id,category_id,product_name,product_introduction,product_price,product_path,on_sale)
 values (2,3,'ペプシコーラ','炭酸の刺激と独特な味わいが、のどの乾きを癒してくれます。',
 270,'/product/2.jpg',true);
 
@@ -391,7 +391,7 @@ values
 (12,10);
 
 insert into products
-(store_id,category_id,product_name,product_introduction,product_price,on_sale)
+(store_id,category_id,product_name,product_introduction,product_price,product_path,on_sale)
 values (3,1,'トマトチーズ牛丼','トマトソースと濃厚なチーズが調和した洋風な味わいの商品です。マイルドなチーズと絡めてお召し上がりください。',
 580,'/product/13.jpg',false);
 
@@ -446,9 +446,9 @@ values
 (13,28);
 
 insert into products
-(store_id,category_id,product_name,product_introduction,product_price,on_sale)
+(store_id,category_id,product_name,product_introduction,product_price,product_path,on_sale)
 values (3,1,'とろ〜り3種のチーズ牛丼','ミックスチーズと、とろ〜りとろけるチーズソースが牛肉とよく絡み、一口食べればチーズのまろやかさが口いっぱいに広がる定番の商品の一つです。',
-580z,'/product/14.jpg';
+580,'/product/14.jpg',true);
 
 insert into options
 (option_name,option_price)
